d5225cfb3c94b686cb9e4160e735cab8e8ea0e9e
Aleksa Cvetic <aleksacvetic71@gmail.com>
2023-07-29 20:35:25 +0200

    basic UI for rps

--- main.js
+++ main.js
@@ -11,68 +11,77 @@ function getComputerChoice(){
     }
 }
 
-function playRound(playerSelection, computerSelection){
-    if(playerSelection == "Rock" && computerSelection =="Rock"){
+function playRound(sign, computerSelection){
+    if(sign == "Rock" && computerSelection =="Rock"){
         let result = "It's a tie! You both picked 'Rock'";
         return result;
     }
-    else if(playerSelection == "Paper" && computerSelection =="Paper"){
+    else if(sign == "Paper" && computerSelection =="Paper"){
         let result = "It's a tie! You both picked 'Paper'";
         return result;
     }
-    else if(playerSelection == "Scissors" && computerSelection =="Scissors"){
+    else if(sign == "Scissors" && computerSelection =="Scissors"){
         let result = "It's a tie! You both picked 'Scissors'";
         return result;
     }
-    else if(playerSelection == "Rock" && computerSelection =="Scissors"){
+    else if(sign == "Rock" && computerSelection =="Scissors"){
         let result = "You win! Rock beats Scissors";
         return result;
     }
-    else if(playerSelection == "Paper" && computerSelection =="Rock"){
+    else if(sign == "Paper" && computerSelection =="Rock"){
         let result = "You win! Paper beats Rock";
         return result;
     }
-    else if(playerSelection == "Scissors" && computerSelection =="Paper"){
+    else if(sign == "Scissors" && computerSelection =="Paper"){
         let result = "You win! Scissors beats Paper";
         return result;
     }
-    else if(playerSelection == "Rock" && computerSelection =="Paper"){
+    else if(sign == "Rock" && computerSelection =="Paper"){
         let result = "You lose! Rock loses to Paper";
         return result;
     }
-    else if(playerSelection == "Paper" && computerSelection =="Scissors"){
+    else if(sign == "Paper" && computerSelection =="Scissors"){
         let result = "You lose! Paper loses to Scissors";
         return result;
     }
-    else if(playerSelection == "Scissors" && computerSelection =="Rock"){
+    else if(sign == "Scissors" && computerSelection =="Rock"){
         let result = "You lose! Scissors loses to Rock";
         return result;
     }
 }
+let teamHuman = 0;
+let teamComputer = 0;
 
-function game(){
-    let teamHuman = 0;
-    let teamComputer = 0;
-    do{
-        let playerSelection = prompt("Choose 'Rock', 'Paper' or 'Scissors'!");
+function game(sign){
+    
         let computerSelection = getComputerChoice();
-        let answer = playRound(playerSelection,computerSelection);
+        let answer = playRound(sign,computerSelection);
         console.log(answer);
         let odg = answer.slice(0,7);
         if(odg=="You win"){
             teamHuman++;
             console.log(`Score: ${teamHuman} - ${teamComputer}`);
+            document.getElementById('scoreboard').textContent=`${teamHuman} - ${teamComputer}`;
+            if(teamHuman==5) {
+                document.getElementById("results").textContent="Congrats, you win!!!";
+                teamHuman=0;
+                teamComputer=0;
+            }
         }
         else if(odg=="You los"){
             teamComputer++;
             console.log(`Score: ${teamHuman} - ${teamComputer}`);
+            document.getElementById('scoreboard').textContent=`${teamHuman} - ${teamComputer}`;
+            if(teamComputer==5){
+                 document.getElementById("results").textContent="Aww shucks, you lost :(";
+                 teamHuman=0;
+                 teamComputer=0;
+            }
         }
         else{
             console.log(`Score: ${teamHuman} - ${teamComputer}`);
         }
-    }while(teamHuman < 5 && teamComputer < 5)
 }
 
 
 
-game();
